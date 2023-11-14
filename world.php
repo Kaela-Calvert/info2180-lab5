6d4b4429c8a1966d390928a479d064055c3620ee
Kaela Calvert <kaelacalvert14@gmail.com>
2023-11-13 21:37:57 -0500

    Edited Files

--- world.php
+++ world.php
@@ -17,10 +17,26 @@ if (isset($_GET['country'])) {
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+  <thead>
+    <tr>
+      <th>Country Name</th>
+      <th>Continent</th>
+      <th>Independence Year</th>
+      <th>Head of State</th>
+    </tr>
+  </thead>
+    <tbody>
+      <?php foreach ($results as $row): ?>
+        <tr>
+          <td><?= $row['name']; ?></td>
+          <td><?= $row['continent']; ?></td>
+          <td><?= $row['independence_year']; ?></td>
+          <td><?= $row['head_of_state']; ?></td>
+        </tr>
+      <?php endforeach; ?>
+  </tbody>
+</table>
+
 
 
